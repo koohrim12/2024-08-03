e18fdaa4c828c65c809d4d6e746fc6ecc77c09c2
구하림 <166689288+koohrim12@users.noreply.github.com>
2024-08-03 19:10:19 +0900

    삭제

--- frontnext/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import Image from 'next/image';
-import Tabs from '../app/lib/tabs';
-import RenderLoginFormSection from './ui/RenderLoginFormSection';
-export default function Home() {
-  return <RenderLoginFormSection />;
-}
