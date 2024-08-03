a15309589cceb131e16eff314826b01fe53d931a
구하림 <166689288+koohrim12@users.noreply.github.com>
2024-08-03 19:01:05 +0900

    빌딩

---
+++ frontnext/src/app/page.tsx
@@ -0,0 +1,6 @@
+import Image from 'next/image';
+import Tabs from '../app/lib/tabs';
+import RenderLoginFormSection from './ui/RenderLoginFormSection';
+export default function Home() {
+  return <RenderLoginFormSection />;
+}
